3886f1b92787508ff91e853eddabf918f58d15dc
EnesElma <elmaenes123@gmail.com>
2019-05-03 20:00:03 +0300

    int dizisine dönüştürdüğümüz iki sayıyı tersten başlayıp toplayarak sonucu bir vektöre atamayı yapan for döngüsünü ekledik, ardından başka bir for döngüsüyle vektörü ekrana yazdırıp işlem sonucunu bulmuş olduk

--- Sum of very big integer.cpp
+++ Sum of very big integer.cpp
@@ -1,7 +1,10 @@
+/*
+ * Enes Elma
+ * 1306120054
+ */
 #include <iostream>
 #include <vector>
 #include <cstring>
-#include <string>
 using namespace std;
 
 /*
@@ -15,37 +18,80 @@ int main(){
 
     cout<<"1.sayiyi giriniz: \n";
     cin>>bigNumber;
-    int sayiBoyutu=bigNumber.size();
+    int sayiBoyutu1=bigNumber.size();
+    int maxBoyut=sayiBoyutu1;            //hangi sayi büyükse ona göre döngü oluşturacağız
 
-    char stringArray1[sayiBoyutu+1];     //stringi arraya dönüştürdükten sonra içine atmak için karakter dizisi tanımlıyoruz
+    char stringArray1[sayiBoyutu1+1];     //stringi arraya dönüştürdükten sonra içine atmak için karakter dizisi tanımlıyoruz
     strcpy(stringArray1,bigNumber.c_str());    //c_str() ile stringi arraya dönüştürüp stringArray içine kopyalıyoruz
 
-    static int *sayi1=new int[sayiBoyutu+1];       //char dizisini int e dönüştürüp içine atmak için int arrayi oluşturuyoruz, array döndürmek için static yapmak lazım
+    static int *sayi1=new int[sayiBoyutu1+1];       //char dizisini int e dönüştürüp içine atmak için int arrayi oluşturuyoruz, array döndürmek için static yapmak lazım
 
-    for(int i=0;i<sayiBoyutu;i++){
+    for(int i=0;i<sayiBoyutu1;i++){
         sayi1[i]=stringArray1[i]-48;  //stringArray içindeki değerleri tek tek integer a dönüştürüyoruz.
     }
 
 
     cout<<"2.sayiyi giriniz: \n";
     cin>>bigNumber;
-    sayiBoyutu= bigNumber.size();
+    int sayiBoyutu2= bigNumber.size();
+    if(sayiBoyutu2>maxBoyut) maxBoyut=sayiBoyutu2;
 
-    char stringArray2[sayiBoyutu+1];     //stringi arraya dönüştürdükten sonra içine atmak için karakter dizisi tanımlıyoruz
+    char stringArray2[sayiBoyutu2+1];     //stringi arraya dönüştürdükten sonra içine atmak için karakter dizisi tanımlıyoruz
     strcpy(stringArray2,bigNumber.c_str());    //c_str() ile stringi arraya dönüştürüp stringArray içine kopyalıyoruz
 
-    static int *sayi2=new int[sayiBoyutu+1];       //char dizisini int e dönüştürüp içine atmak için int arrayi oluşturuyoruz, array döndürmek için static yapmak lazım
+    static int *sayi2=new int[sayiBoyutu2+1];       //char dizisini int e dönüştürüp içine atmak için int arrayi oluşturuyoruz, array döndürmek için static yapmak lazım
 
-
-    for(int i=0;i<sayiBoyutu;i++){
+    for(int i=0;i<sayiBoyutu2;i++){
         sayi2[i]=stringArray2[i]-48;  //stringArray içindeki değerleri tek tek integer a dönüştürüyoruz.
     }
 
-    cout<< sizeof(sayi1);
+    vector <int> toplamDizi;
+    int elde=0;
+    int toplam;
+    for(int i=0;i<maxBoyut;i++){        //iki dizinin elemanlarını toplayıp toplamDizi vektörüne push ediyoruz
 
+        if(sayiBoyutu1!=0 && sayiBoyutu2!=0) {
+            toplam = sayi1[sayiBoyutu1 - 1] + sayi2[sayiBoyutu2 - 1] + elde;
+            elde = 0;
+            if (toplam > 9) {
+                elde = 1;
+                toplam = toplam % 10;
+            }
+            toplamDizi.push_back(toplam);
+            sayiBoyutu1--;
+            sayiBoyutu2--;
+        }
+        if(sayiBoyutu2==0 && sayiBoyutu1!=0){
+            toplam = sayi1[sayiBoyutu1 - 1]+elde;
+            elde = 0;
+            if (toplam > 9) {
+                elde = 1;
+                toplam = toplam % 10;
+            }
+            toplamDizi.push_back(toplam);
+            sayiBoyutu1--;
+        }
+        if(sayiBoyutu1==0 && sayiBoyutu2!=0){
+            toplam = sayi2[sayiBoyutu2 - 1]+elde;
+            elde = 0;
+            if (toplam > 9) {
+                elde = 1;
+                toplam = toplam % 10;
+            }
+            toplamDizi.push_back(toplam);
+            sayiBoyutu2--;
+        }
 
+    }
 
+    if(elde==1){    //son toplama işleminde elde kaldıysa eğer onu da toplamDizi vektörüne push ediyoruz.
+        maxBoyut++;
+        toplamDizi.push_back(elde);
+    }
 
+    for(int i=maxBoyut;i>0;i--){    //vektöre tersten ekleme yaptığımız için eğer vektörü yeniden ters çevirip yazdırırsak işlem sonucunu bulmuş oluruz
+        cout<<toplamDizi[i-1];
+    }
 
     return 0;
 }
